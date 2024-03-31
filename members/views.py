d408a24f8651daf17f748e42d65821577d230351
PaulKaiba <njaupaul6@gmail.com>
2024-03-31 20:18:37 +0300

    Adding admin panel

--- members/views.py
+++ members/views.py
@@ -27,4 +27,13 @@ def main(request):
     return HttpResponse(template.render())
 
 
+def testing(request):
+    template=loader.get_template('template.html')
+    context = {
+        'fruits':['Apple','Banana','Cherry']
+    }
+
+    return HttpResponse(template.render(context,request))
+
+
 # Create your views here.
